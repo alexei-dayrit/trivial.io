0bb89f6956ec2935c77a7b6b4bfef3116b56f7e6
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-03-01 19:39:48 +0000

    Add input buttons for difficulty levels. #1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,67 @@
 /* global data */
 /* exported data */
 
+var $mainHeading = document.querySelector('#main-heading');
 var $gameForm = document.querySelector('form');
 var $categoryWrapper = document.querySelector('#category-wrapper');
 
-// TESTING
+// HANDLE CATEGORY CLICKS
 function handleCategoryClicks(event) {
-  // console.log('event:', event);
-  // console.log('event.target:', event.target);
-  // console.log('event.target.tagName:', event.target.tagName);
   if (event.target.tagName === 'INPUT') {
-    // console.log('INPUT BUTTON:', event.target);
+    $categoryWrapper.setAttribute('class', 'row hidden');
+    renderDifficuly();
   }
 }
-
+// CATEGORY CLICK LISTENER
 $categoryWrapper.addEventListener('click', handleCategoryClicks);
 
+// HANDLE FORM
 function handleGameForm(event) {
   event.preventDefault();
 }
-
+// FORM SUBMIT LISTENER
 $gameForm.addEventListener('submit', handleGameForm);
+
+// RENDER DIFFICULTY
+function renderDifficuly() {
+  $mainHeading.textContent = 'Select Difficulty';
+
+  var $difficultyWrapper = document.createElement('div');
+  $difficultyWrapper.setAttribute('id', 'difficulty-wrapper');
+  $difficultyWrapper.setAttribute('class', 'row justify-center');
+  $gameForm.appendChild($difficultyWrapper);
+
+  var $easyDiv = document.createElement('div');
+  $easyDiv.setAttribute('class', 'col-sm-fifth flex justify-center');
+  $difficultyWrapper.appendChild($easyDiv);
+
+  var $medDiv = document.createElement('div');
+  $medDiv.setAttribute('class', 'col-sm-fifth flex justify-center');
+  $difficultyWrapper.appendChild($medDiv);
+
+  var $hardDiv = document.createElement('div');
+  $hardDiv.setAttribute('class', 'col-sm-fifth flex justify-center');
+  $difficultyWrapper.appendChild($hardDiv);
+
+  var $easyButton = document.createElement('input');
+  $easyButton.setAttribute('type', 'button');
+  $easyButton.setAttribute('name', 'easy');
+  $easyButton.setAttribute('class', 'difficulty-button text-capitalize');
+  $easyButton.setAttribute('value', 'easy');
+  $easyDiv.appendChild($easyButton);
+
+  var $medButton = document.createElement('input');
+  $medButton.setAttribute('type', 'button');
+  $medButton.setAttribute('name', 'med');
+  $medButton.setAttribute('class', 'difficulty-button text-capitalize');
+  $medButton.setAttribute('value', 'med');
+  $medDiv.appendChild($medButton);
+
+  var $hardButton = document.createElement('input');
+  $hardButton.setAttribute('type', 'button');
+  $hardButton.setAttribute('name', 'hard');
+  $hardButton.setAttribute('class', 'difficulty-button text-capitalize');
+  $hardButton.setAttribute('value', 'hard');
+  $hardDiv.appendChild($hardButton);
+
+}
